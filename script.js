0f42c3a562586d2bc1d87bbe16f4f23852a09321
Dmitry <roninde96@gmail.com>
2023-04-14 12:23:21 +0300

    added animation to carousel

--- script.js
+++ script.js
@@ -19,14 +19,64 @@ function properIndex() {
     }
 
 };
+function addAnimationClass(isForward) {
+    if (isForward) {
+        document.querySelector('.hiddenImg').classList.add('hiddenImg-animate-forward');
+        document.querySelector('.mainImg').classList.add('mainImg-animate-forward');
+        document.querySelector('.leftImg').classList.add('leftImg-animate-forward');
+        document.querySelector('.rightImg').classList.add('rightImg-animate-forward');
+    } else {
+        document.querySelector('.hiddenImg').classList.add('hiddenImg-animate-backward');
+        document.querySelector('.mainImg').classList.add('mainImg-animate-backward');
+        document.querySelector('.leftImg').classList.add('leftImg-animate-backward');
+        document.querySelector('.rightImg').classList.add('rightImg-animate-backward');
+    }
+}
+
+function removeAnimationClass(isForward) {
+    if (isForward) {
+        document.querySelector('.hiddenImg').classList.remove('hiddenImg-animate-forward');
+        document.querySelector('.mainImg').classList.remove('mainImg-animate-forward');
+        document.querySelector('.leftImg').classList.remove('leftImg-animate-forward');
+        document.querySelector('.rightImg').classList.remove('rightImg-animate-forward');
+    } else {
+        document.querySelector('.hiddenImg').classList.remove('hiddenImg-animate-backward');
+        document.querySelector('.mainImg').classList.remove('mainImg-animate-backward');
+        document.querySelector('.leftImg').classList.remove('leftImg-animate-backward');
+        document.querySelector('.rightImg').classList.remove('rightImg-animate-backward');
+    }
+}
+
+function changeImageClass(isForward) {
+    if (isForward) {
+        document.querySelector('.hiddenImg-animate-forward').className = 'rightImg';
+        document.querySelector('.mainImg-animate-forward').className = 'leftImg';
+        document.querySelector('.leftImg-animate-forward').className = 'hiddenImg';
+        document.querySelector('.rightImg-animate-forward').className = 'mainImg';
+    } else {
+        document.querySelector('.hiddenImg-animate-backward').className = 'leftImg';
+        document.querySelector('.mainImg-animate-backward').className = 'rightImg';
+        document.querySelector('.leftImg-animate-backward').className = 'mainImg';
+        document.querySelector('.rightImg-animate-backward').className = 'hiddenImg';
+    }
+}
+
+function animate(isForward) {
+    addAnimationClass(isForward);
+    setTimeout(changeImageClass, 500, isForward);
+    setTimeout(removeAnimationClass, 500, isForward);
+}
+                
+
 
 function shiftImages(isForward) {
     for (i = 0; i < index.length; i++) {
         index[i] = isForward ? (index[i] + 1) : (index[i] - 1);
     }
     properIndex();
-    updateSrc();
-    console.log(index);
+    animate(isForward);
+    setTimeout(updateHidden, 500);
+    // console.log(index);
 }
 
 function createButton(buttonName, isForward) {
@@ -42,11 +92,16 @@ function createImageBox(boxclass) {
     container.appendChild(box);
 }
 
-function updateSrc() {
-    document.querySelector('.hiddenImg').setAttribute('src', imgArray[index[0]]);
-    document.querySelector('.leftImg').setAttribute('src', imgArray[index[1]]);
-    document.querySelector('.mainImg').setAttribute('src', imgArray[index[2]]);
-    document.querySelector('.rightImg').setAttribute('src', imgArray[index[3]]);
+function setSrc() {
+    document.querySelector('.leftImg').setAttribute('src', imgArray[index[0]]);
+    document.querySelector('.mainImg').setAttribute('src', imgArray[index[1]]);
+    document.querySelector('.rightImg').setAttribute('src', imgArray[index[2]]);
+    document.querySelector('.hiddenImg').setAttribute('src', imgArray[index[3]]);
+}
+
+function updateHidden() {
+    document.querySelector('.hiddenImg').setAttribute('src', imgArray[index[3]]);
+
 }
 
 function createCarousel() {
@@ -55,7 +110,7 @@ function createCarousel() {
     createImageBox('leftImg');
     createImageBox('rightImg');
     createImageBox('mainImg');
-    updateSrc();
+    setSrc();
 
     createButton('shiftLeft', 0);
 
